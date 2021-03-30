4337fa67da03d2ec41bcc8fb63fdd51d6daf6175
angel19951 <2384@holbertonschool.com>
2021-03-30 13:16:15 +0000

    Adding delete and all updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    new_dict[key] = value
+            return new_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -24,6 +30,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Delete a specific object"""
+        if obj:
+            obj_del = obj.__class__.__name__ + '.' + obj.id
+            if obj_del in self.__objects:
+                del self.__objects[obj_del]
 
     def reload(self):
         """Loads storage dictionary from file"""
