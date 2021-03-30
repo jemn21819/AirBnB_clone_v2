c8f514367a76e121347f5a4c2dc7e61eadc34a12
angel19951 <2384@holbertonschool.com>
2021-03-30 18:55:29 +0000

    Fixing pep8 complaints

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -30,7 +30,8 @@ class DBStorage:
                                       format(MySQL_user,
                                              MySQL_password,
                                              MySQL_host,
-                                             MySQL_database, pool_pre_ping=True))
+                                             MySQL_database,
+                                             pool_pre_ping=True))
         Base.metadata.create_all(self.__engine)
         if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
@@ -63,7 +64,8 @@ class DBStorage:
             self.__session.delete(obj)
 
     def reload(self):
-        """create all tables in the database (feature of SQLAlchemy)"""
+        """
+        create all tables in the database (feature of SQLAlchemy)"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
                 bind=self.__engine, expire_on_commit=False)
