921607ba454d50a5596655ee51f565e9fee478c4
angel19951 <2384@holbertonschool.com>
2021-03-30 17:49:07 +0000

    Updating classes for SQLAlchemy

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel
-
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 class Amenity(BaseModel):
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
