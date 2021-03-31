82929b2ea3d72f0c92c456dc2b06dd6fb60d3951
angel19951 <2384@holbertonschool.com>
2021-03-31 15:03:07 +0000

    Updating file storage for better handling

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
@@ -10,13 +22,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls:
+        if cls is not None:
             new_dict = {}
             for key, value in self.__objects.items():
-                if cls.__name__ == value.__class__.__name__:
+                if cls == value.__class__ or cls == value == value.__name__:
                     new_dict[key] = value
             return new_dict
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -33,34 +45,17 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Delete a specific object"""
-        if obj:
-            try:
-                obj_del = "{}.{}".format(type(obj).__name__, obj.id)
+        if obj is not None:
+            obj_del = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if obj_del in self.__objects:
                 del self.__objects[obj_del]
-                self.save()
-            except:
-                pass
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+            for key, val in temp.items():
+                self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
