168316f52c89152d86eff4c482dc82a7c0884e6d
angel19951 <2384@holbertonschool.com>
2021-03-31 14:14:54 +0000

    Updating state class for file handlign

--- models/state.py
+++ models/state.py
@@ -7,21 +7,32 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from models.city import City
 from models.place import Place
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        """getter attribute cities that returns the list of City
-        instances with state_id equals to the current State.id"""
-        cities_list = []
-        dict_cities = models.storage.all(City)
-        for key, city in dict_cities.items():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+    def __init__(self, *args, **kawrgs):
+        """
+        Initalization of State class
+        """
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City
+            instances with state_id equals to the current State.id"""
+            cities_list = []
+            dict_cities = models.storage.all(City)
+            for key, city in dict_cities.items():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
