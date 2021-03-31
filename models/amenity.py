b1b176624231e9e141ad1018cd1dc4138a1d394b
angel19951 <2384@holbertonschool.com>
2021-03-31 13:52:48 +0000

    Updating amenity class for file handlign

--- models/amenity.py
+++ models/amenity.py
@@ -5,10 +5,23 @@ from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity',
-                                   backref='amenities')
+    """
+    Amenity class creation
+    """
+    if getenv('HBNB_TYPE_STORAGE' == 'db'):
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       backref='amenities')
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Amenity class initialization
+        """
+        super().__init__(*args, **kwargs)
