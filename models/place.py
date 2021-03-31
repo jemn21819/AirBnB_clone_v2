4cd09fb7258ebc9f98efcbc3c836c120a07c2fc7
angel19951 <2384@holbertonschool.com>
2021-03-31 14:05:59 +0000

    Updating place class for file handlign

--- models/place.py
+++ models/place.py
@@ -10,8 +10,9 @@ from models.review import Review
 from os import getenv
 from models.amenity import Amenity
 
+
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity = Table('place_amenity', metadata,
+    place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id'),
                                  primary_key=True, nullable=False),
@@ -23,8 +24,8 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
@@ -35,12 +36,30 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        amenity_ids = []
         reviews = relationship('Review', cascade='all, delete',
                                backref='place')
         amenities = relationship('Amenity', secondary='place_amenity',
                                  backref='places', viewonly=False)
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initilization of the place class
+        """
+        super().__init__(*args, **kwargs)
+
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
         @property
         def reviews(self):
             """
@@ -76,3 +95,4 @@ class Place(BaseModel, Base):
                 self.amenity_ids.append(obj.id)
             else:
                 pass
+
