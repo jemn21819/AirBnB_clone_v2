169d6e031b17fa66c54d1aa2c69429a15845b7dc
angel19951 <2384@holbertonschool.com>
2021-03-31 14:08:20 +0000

    Updating review  class for file handlign

--- models/review.py
+++ models/review.py
@@ -4,11 +4,18 @@ import models
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from os import getenv
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'reviews'
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        def __init__(self, *args, **kwargs):
+            """
+            Initialization Review class
+            """
+            super().__init__(*args, **kwargs)
