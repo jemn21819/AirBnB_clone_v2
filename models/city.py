9c8071d491fbc0d9b3d6a6e2e4a20cc3f251f93d
angel19951 <2384@holbertonschool.com>
2021-03-31 13:56:29 +0000

    Updating city class for file handlign

--- models/city.py
+++ models/city.py
@@ -5,12 +5,21 @@ import sqlalchemy
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade='all, delete', backref='cities')
+    else:
+        name = ""
+        state_id = ""
 
-    places = relationship('Place', cascade='all, delete', backref='cities')
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization of city class
+        """
+        super().__init__(*args, **kwargs)
