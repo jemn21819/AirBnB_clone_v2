aa014371f37a3404730cb88577a703c311c49c67
angel19951 <2384@holbertonschool.com>
2021-03-31 14:39:27 +0000

    Fixing arguments errors

--- models/amenity.py
+++ models/amenity.py
@@ -12,7 +12,7 @@ class Amenity(BaseModel, Base):
     """
     Amenity class creation
     """
-    if getenv('HBNB_TYPE_STORAGE' == 'db'):
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
         place_amenities = relationship('Place', secondary='place_amenity',
