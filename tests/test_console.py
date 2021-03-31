bb4aa549b730e6ed2cea64ea98a9c1bc4fbadd89
angel19951 <2384@holbertonschool.com>
2021-03-31 02:04:42 +0000

    Fixing pep8 complaints

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,6 +17,7 @@ class TestConsole(unittest.TestCase):
     """
     Test console with unit test
     """
+
     @classmethod
     def setUpClass(cls):
         """
@@ -44,6 +45,7 @@ class TestConsole(unittest.TestCase):
         """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['tests/test_console.py'])
+
     def testDocstring(self):
         """
         Test docstring model for console
@@ -61,4 +63,3 @@ class TestConsole(unittest.TestCase):
                          "HBNBCommand class needs a docstring.")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring.")
-
