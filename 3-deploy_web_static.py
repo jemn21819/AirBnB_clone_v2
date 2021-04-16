172168d6c2c0525e852b911ca7dfd0efc0152018
James <jmartinez21834@stu.nuc.edu>
2021-04-16 03:43:09 +0000

    adding file 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+ (based on the file 1-pack_web_static.py) that distributes an archive to your
+ web servers, using the function do_deploy
+"""
+
+from fabric.api import put, run, env, local
+from os.path import exists, isdir
+from datetime import datetime
+env.hosts = ['35.231.81.64', '34.74.73.209']
+
+
+def do_pack():
+    """ return the archive path if the archive has been correctly generated."""
+    try:
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_path = "versions/web_static_{}.tgz".format(time)
+        local("tar -cvzf {} web_static".format(file_path))
+        return file_path
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to your web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
