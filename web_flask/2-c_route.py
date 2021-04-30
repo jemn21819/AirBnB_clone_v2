40353363dbe6d30a8be4c6afa58e524fbad3e586
James <jmartinez21834@stu.nuc.edu>
2021-04-30 16:31:56 +0000

    adding task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ display Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """ display C  followed by the value of the text variable"""
+    return "C {}".format(text).replace('_', ' ')
+
+
+if __name__ == '__main__':
+    """application must be listening on"""
+    app.run(host='0.0.0.0', port='5000')
