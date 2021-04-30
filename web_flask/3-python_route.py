f654bed6b2e37c219649092527c60e3706e08496
James <jmartinez21834@stu.nuc.edu>
2021-04-30 22:43:42 +0000

    adding task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ display Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """ display C  followed by the value of the text variable"""
+    return "C {}".format(text).replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text='is cool'):
+    """display Python , followed by the value of the text variable
+    default value of text is 'is cool'"""
+    return 'Python {}'.format(text).replace('_', ' ')
+
+
+if __name__ == '__main__':
+    """application must be listening on"""
+    app.run(host='0.0.0.0', port='5000')
